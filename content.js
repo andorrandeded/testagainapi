475f2b02aab1557d3790a553d37c1ae2ad7c15f3
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-01-19 04:23:05 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,4 +1,3 @@
-<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 $.getJSON(
   'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=5e2acefe7351c261f2c6662b438d2739',
 function(data){
