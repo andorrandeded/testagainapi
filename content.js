3125abc1dc7fa6ef1f4407485fc841c3e332ec6e
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-01-19 04:17:49 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,6 +1,6 @@
 <script>
-$.getJSON(
-  'https://api.wheretheiss.at/v1/satellites/25544',
+getJSON(
+  'https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=5e2acefe7351c261f2c6662b438d2739',
 function(data){
   console.log(data);
 }
