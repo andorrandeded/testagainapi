6a2c73dcc618ea1575b211eed79a95ff6c7d7676
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-02-23 22:52:04 +0100

    Update content.js

--- content.js
+++ content.js
@@ -1,8 +1,8 @@
 
-a = document.getElementById("take").value;
-setTimeout (function(){
-cookies.set("name",a);
-},2000);
+
+
 function alertCookie() {
-document.getElementById("take1").value=cookies.get("name");
+a = document.getElementById("take").value;
+cookies.set("name",a);
+
 }
