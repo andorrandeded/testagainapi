3ad8e72203b61259c39e0c71ce213f4d3db3ce04
andorrandeded <59179358+andorrandeded@users.noreply.github.com>
2020-02-27 23:46:03 +0100

    Update content.js

--- content.js
+++ content.js
@@ -2,11 +2,7 @@
 
 
 function alertCookie() {
-cookievalue=escape(document.getElementById("take").value);
-var now = new Date();
-  var time = now.getTime();
-  var expireTime = time + 1000*36000;
-  now.setTime(expireTime);
-  var tempExp = 'Wed, 31 Oct 2012 08:50:17 GMT';
-  document.cookie = 'cookie=ok;expires='+now.toGMTString()+';path=/';
+document.getElementById("take").value;
+console.log(firebase);  
+
 }
